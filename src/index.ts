753c80f3736cdbe41f508ad4cd48ebb4187bafb4
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2024-03-17 16:55:00 +0100

    use openai api key env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import OpenAI from "openai";
 
-const openai = new OpenAI();
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 async function main() {
   const stream = await openai.chat.completions.create({
