7b7a78c8261840cce48d8de23e2fc014a1859580
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2024-05-06 11:14:59 +0200

    make dev works

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,4 +1,3 @@
-import "dotenv/config";
 import { ChatOpenAI } from "@langchain/openai";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { StringOutputParser } from "@langchain/core/output_parsers";
